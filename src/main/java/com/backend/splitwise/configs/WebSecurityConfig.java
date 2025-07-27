4bb31770c520efb8d608c9dcd74d9e9bd58f7173
Rahul Mahato <mahatorahul2901@gmail.com>
2025-07-27 16:27:54 +0530

    Config: Update app and security configurations

--- src/main/java/com/backend/splitwise/configs/WebSecurityConfig.java
+++ src/main/java/com/backend/splitwise/configs/WebSecurityConfig.java
@@ -1,4 +1,35 @@
 package com.backend.splitwise.configs;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private static final String[] publicRoutes = {
+        "/error", "/auth/*", "/users/**", "/groups/**"
+    };
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(publicRoutes).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionConfig -> sessionConfig
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
+    }
 }
