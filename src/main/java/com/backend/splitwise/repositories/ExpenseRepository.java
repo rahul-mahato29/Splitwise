e3746e09dccdf26441eb6b26d5324f9f22f9dc46
Rahul Mahato <mahatorahul2901@gmail.com>
2025-07-29 23:54:03 +0530

    Chore: Modify ExpenseRepository for extended querying capabilities

--- src/main/java/com/backend/splitwise/repositories/ExpenseRepository.java
+++ src/main/java/com/backend/splitwise/repositories/ExpenseRepository.java
@@ -1,4 +1,9 @@
 package com.backend.splitwise.repositories;
 
-public interface ExpenseRepository {
+import com.backend.splitwise.entities.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 }
